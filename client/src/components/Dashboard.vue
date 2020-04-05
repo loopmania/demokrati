270d777eb890bdea9b6cec4c7d48153fe44ab5e3
loopMania <johnlan@kth.se>
2020-04-05 15:36:33 +0200

    fixed dashboard grid

--- client/src/components/Dashboard.vue
+++ client/src/components/Dashboard.vue
@@ -1,12 +1,38 @@
 <template>
     <div>
-        Dashboard
+        <v-card
+        flat
+        max-width="80%"
+        fill-height
+        class="mx-auto">
+            <v-container>
+                <v-row
+                >
+                    <v-col
+                    class="d-flex align-stretch">
+                        <!-- antalet medlemmar -->
+                    </v-col>
+                    <v-col
+                    class="d-flex align-stretch">
+                        <!-- Aktiv omröstning -->
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col
+                    class="d-flex align-stretch">
+                        <!-- Misc funktioner -->
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-card>
     </div>
 </template>
 <script>
 export default {
-    name: 'Dashboard'
-
+    name: 'Dashboard',
+    components: {
+        // add components
+    }
     // kicka alla!
     // statistik :)
 }
